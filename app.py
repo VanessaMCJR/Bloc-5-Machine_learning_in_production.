c048def7745cab53a901ba87a3046bb7e53525b1
Vanessa <43271469+VanessaMCJR@users.noreply.github.com>
2022-09-10 10:25:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import joblib, requests
 import pandas as pd
 
 
-
-
 app = Flask(__name__)
 
 
@@ -14,32 +12,36 @@ def index():
     return render_template("index.html")
 
 
-
 @app.route("/predict", methods=["POST"])
 
 
 def prediction_quality_wine():
+    
     # Check if request has a JSON content
     if request.json and request.method == "POST":
+        
         # Get the JSON as dictionnary
         req = request.get_json()
+        
         # Check mandatory key
         if "input" in req.keys():
-            # Load model
             predict1 =[]
+            
             X = pd.DataFrame(req["input"])
+            # Load model
             classifier = joblib.load("model.joblib")
             # Predict
             prediction = classifier.predict(X)
-            # Return the result as JSON but first we need to transform the
-            # result so as to be serializable by jsonify
+            
+            # We transform the predictions into a python list
 
             for i in range(len(prediction+1)):
                 predict1.append(str(prediction[i]))
 
-
-            
             prediction1 = [float(p)for p in predict1]
+            
+            # We display the results in a json dictionary
+            
             return jsonify({"predict": prediction1}), 200
     return jsonify({"msg": "Error: not a JSON or no email key in your request"})
 
